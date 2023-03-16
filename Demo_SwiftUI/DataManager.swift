4f9cbb4afe023d9ff0b8eee10d0bb6ef0a7c9f30
sangtt18 <SangTt18@fpt.com.vn>
2023-03-16 20:49:59 +0700

    init project

---
+++ Demo_SwiftUI/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Demo_SwiftUI
+//
+//  Created by Sang Truong on 16/03/2023.
+//
+
+import Foundation
---
+++ Demo_SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIApp.swift
+//  Demo_SwiftUI
+//
+//  Created by Sang Truong on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
